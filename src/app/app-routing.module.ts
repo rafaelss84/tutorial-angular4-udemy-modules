741d81f373ce0ff2647b999146abf9d31db601b6
RAFAEL SOUZA DA SILVA <rafaelss@ciasc.sc.gov.br>
2017-07-28 13:09:55 -0300

    Aula 132: Externalizando a configuração de Routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from "@angular/core";
+import { HomeComponent } from "app/home/home.component";
+import { UsersComponent } from "app/users/users.component";
+import { UserComponent } from "app/users/user/user.component";
+import { ServersComponent } from "app/servers/servers.component";
+import { ServerComponent } from "app/servers/server/server.component";
+import { EditServerComponent } from "app/servers/edit-server/edit-server.component";
+import { PageNotFoundComponent } from "app/page-not-found/page-not-found.component";
+import { Routes, RouterModule } from "@angular/router";
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent, children: [
+    {path: ':id/:name', component: UserComponent}
+  ]},
+  {path: 'servers', component: ServersComponent, children: [
+    {path: ':id', component: ServerComponent},
+    {path: ':id/edit', component: EditServerComponent}
+  ]},
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
